309dd9ea2d224f21f55886f6bdba0b919dbf3b48
younusaliakash <younusalikash8@gmail.com>
2023-09-26 23:05:29 +0600

    single product info extraction

--- index.mjs
+++ index.mjs
@@ -10,7 +10,28 @@ const allProductsLink = await page.evaluate(() => {
     return [...document.querySelectorAll('.product-thumbnail .woocommerce-loop-product__link')].map(el => el.href)
 })
 
-console.log(allProductsLink)
+const extractProductInfo = (page, selector) => {
+    return page.evaluate((selector) => {
+        return document.querySelector(selector)?.innerText
+    }, selector)
+}
+
+for (let link of allProductsLink) {
+    const page = await browser.newPage()
+    await page.goto(link, { waitUntil: 'networkidle0' })
+
+    await page.waitForSelector('.product_title')
+    // const title = await page.evaluate(() => {
+    //     return document.querySelector('.product_title')?.innerHTML
+    // })
+    const title = await extractProductInfo(page, '.product_title')
+    const description = await extractProductInfo(page, '.woocommerce-product-details__short-description p')
+    const price = await extractProductInfo(page, '.price .amount')
+
+    console.log({ link, title, description, price } )
+
+    await page.close()
+}
 
 await page.close()
 await browser.close()
